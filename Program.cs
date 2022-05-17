64fe7bf5c572025f8c46871cab35e7d05aa79092
Nicole Miguel <nicolemonteiromiguel@gmail.com>
2022-05-16 22:11:23 -0300

    adicionando lab list e lab new

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.Data.Sqlite;
+﻿using Microsoft.Data.Sqlite;
 
 var connection = new SqliteConnection("Data Source=database.db");
 connection.Open();
@@ -11,14 +10,21 @@ command.CommandText = @"
        ram varchar(100) not null,
        processor varchar(100) not null
    );
+
+    CREATE TABLE IF NOT EXISTS Labs(
+        id int not null primary key,
+        number varchar(100) not null,
+        name varchar(100) not null,
+        block varchar(100) not null
+    );
 ";
 command.ExecuteNonQuery();
 
 connection.Close();
 
 //Routing
-var modelName = args [0];
-var modelAction = args [1];
+var modelName = args[0];
+var modelAction = args[1];
 
 if(modelName == "Computer")
 {
@@ -58,6 +64,50 @@ if(modelName == "Computer")
         command.Parameters.AddWithValue("$id", id);
         command.Parameters.AddWithValue("$ram", ram);
         command.Parameters.AddWithValue("$processor", processor);
+
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+}
+
+else if(modelName == "Lab")
+{
+    if(modelAction == "List")
+    {
+        Console.WriteLine("Lab List");
+        connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
+
+        command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Labs;";
+
+        var reader = command.ExecuteReader();
+
+        while(reader.Read())
+        {
+            Console.WriteLine("{0}, {1}, {2}, {3}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3));
+        }
+
+        reader.Close();
+        connection.Close();
+    }
+
+    if(modelAction == "New")
+    {
+        int id = Convert.ToInt32(args[2]);
+        var number = args[3];
+        var name = args[4];
+        var block = args[5];
+
+        connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
+
+        command = connection.CreateCommand();
+        command.CommandText = "INSERT INTO Labs VALUES($id, $number, $name, $block);";
+        command.Parameters.AddWithValue("$id", id);
+        command.Parameters.AddWithValue("$number", number);
+        command.Parameters.AddWithValue("$name", name);
+        command.Parameters.AddWithValue("$block", block);
 
         command.ExecuteNonQuery();
         connection.Close();
